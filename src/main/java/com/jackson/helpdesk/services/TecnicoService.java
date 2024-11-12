2f2c858767df1e5f0bd2e9e36d69036cc4d340cd
JacksonLuiz99 <jacksoncuiaba99@gmail.com>
2024-11-11 23:06:50 -0400

    Endpoint  findAll para tecnico

--- src/main/java/com/jackson/helpdesk/services/TecnicoService.java
+++ src/main/java/com/jackson/helpdesk/services/TecnicoService.java
@@ -1,5 +1,6 @@
 package com.jackson.helpdesk.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,5 +19,9 @@ public class TecnicoService {
 		public Tecnico findById(Integer id) {
 			Optional<Tecnico> obj = repository.findById(id);
 			return obj.orElseThrow(() -> new ObjectnotFoundException("O Objeto não foi encontrado! Id: " + id));
+		}
+
+		public List<Tecnico> findAll() {
+			return repository.findAll();
 		}
 }
