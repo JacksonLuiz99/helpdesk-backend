f06f95b949e986f53118966df4331baa4462b15b
JacksonLuiz99 <jacksoncuiaba99@gmail.com>
2024-11-15 23:20:18 -0400

    Exceção para CPF e EMAIL já cadastrados

--- src/main/java/com/jackson/helpdesk/repositories/PessoaRepository.java
+++ src/main/java/com/jackson/helpdesk/repositories/PessoaRepository.java
@@ -1,9 +1,14 @@
 package com.jackson.helpdesk.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.jackson.helpdesk.domain.Pessoa;
 
+
 public interface PessoaRepository extends JpaRepository<Pessoa, Integer>  {
 
+	Optional<Pessoa> findByCpf(String cpf);
+	Optional<Pessoa> findByEmail(String email);
 }
