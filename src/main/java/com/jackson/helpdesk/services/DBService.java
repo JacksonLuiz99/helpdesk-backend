23d32d1e9ade929e817e5c0efa50077128a25d9d
JacksonLuiz99 <jacksoncuiaba99@gmail.com>
2024-11-19 14:52:44 -0300

    correção de erro no banco usando validação do hibernate

--- src/main/java/com/jackson/helpdesk/services/DBService.java
+++ src/main/java/com/jackson/helpdesk/services/DBService.java
@@ -30,25 +30,25 @@ public class DBService {
 		Tecnico tec1 = new Tecnico(null, "Jackson", "31874636737", "jackson@uol.com", "123");
 		tec1.addPerfil(Perfil.ADMIN);
 		Tecnico tec2 = new Tecnico(null, "Scooby-Doo", "17111468708", "scooby.doo@hotmail.com", "123");
-		Tecnico tec3 = new Tecnico(null, "Fred Flintstone", "92541720491", "fred.flintstone@gmail.com", "123");
+		Tecnico tec3 = new Tecnico(null, "Fred Flintstone", "84598048018", "fred.flintstone@gmail.com", "123");
 		Tecnico tec4 = new Tecnico(null, "Homer Simpson", "28964101391", "homer.simpson@outlook.com", "123");
 		Tecnico tec5 = new Tecnico(null, "Bart Simpson", "94829299428", "bart.simpson@yahoo.com", "123");
-		Tecnico tec6 = new Tecnico(null, "Tom (Tom & Jerry)", "73175110053", "tom.jerry@icloud.com", "123");
-		Tecnico tec7 = new Tecnico(null, "Jerry (Tom & Jerry)", "37118663360", "jerry.jerry@gmail.com", "123");
-		Tecnico tec8 = new Tecnico(null, "Pikachu", "76949366060", "pikachu@outlook.com", "123");
-		Tecnico tec9 = new Tecnico(null, "Bugs Bunny", "32165498765", "bugs.bunny@uol.com", "123");
-		Tecnico tec10 = new Tecnico(null, "Daffy Duck", "01926762703", "daffy.duck@hotmail.com", "123");
+		Tecnico tec6 = new Tecnico(null, "Tom (Tom & Jerry)", "73414030047", "tom.jerry@icloud.com", "123");
+		Tecnico tec7 = new Tecnico(null, "Jerry (Tom & Jerry)", "54567179056", "jerry.jerry@gmail.com", "123");
+		Tecnico tec8 = new Tecnico(null, "Pikachu", "85364725022", "pikachu@outlook.com", "123");
+		Tecnico tec9 = new Tecnico(null, "Bugs Bunny", "80348021070", "bugs.bunny@uol.com", "123");
+		Tecnico tec10 = new Tecnico(null, "Daffy Duck", "57678561091", "daffy.duck@hotmail.com", "123");
 		
 		Cliente cli1 = new Cliente(null, "Luiz", "10414231104", "luiz@gmail.com", "123");
 		Cliente cli2 = new Cliente(null, "Maria", "67442322310", "maria@outlook.com", "123");
-		Cliente cli3 = new Cliente(null, "João", "48911372749", "joao@yahoo.com", "123");
+		Cliente cli3 = new Cliente(null, "João", "21022312081", "joao@yahoo.com", "123");
 		Cliente cli4 = new Cliente(null, "Mickey Mouse", "80523674686", "mickey.mouse@gmail.com", "123");
 		Cliente cli5 = new Cliente(null, "Minnie Mouse", "50865652279", "minnie.mouse@uol.com", "123");
-		Cliente cli6 = new Cliente(null, "Donald Duck", "61644636298", "donald.duck@icloud.com", "123");
-		Cliente cli7 = new Cliente(null, "Goofy", "05713872688", "goofy@outlook.com", "123");
-		Cliente cli8 = new Cliente(null, "Kalil Baract", "70915212340", "kalil.baract@gmail.com", "123");
-		Cliente cli9 = new Cliente(null, "Tom Cat", "68885048676", "tom.cat@yahoo.com", "123");
-		Cliente cli10 = new Cliente(null, "Jerry Mouse", "76263146893", "jerry.mouse@bol.com.br", "123");
+		Cliente cli6 = new Cliente(null, "Donald Duck", "27986926015", "donald.duck@icloud.com", "123");
+		Cliente cli7 = new Cliente(null, "Goofy", "09956060054", "goofy@outlook.com", "123");
+		Cliente cli8 = new Cliente(null, "Kalil Baract", "44909088067", "kalil.baract@gmail.com", "123");
+		Cliente cli9 = new Cliente(null, "Tom Cat", "16427231006", "tom.cat@yahoo.com", "123");
+		Cliente cli10 = new Cliente(null, "Jerry Mouse", "67460320005", "jerry.mouse@bol.com.br", "123");
 
 
 		Chamado c1 = new Chamado(null, Prioridade.MEDIA, Status.ANDAMENTO, "Chamado 01", "Primeiro chamado", tec1, cli1);
@@ -63,7 +63,7 @@ public class DBService {
 		Chamado c10 = new Chamado(null, Prioridade.MEDIA, Status.ANDAMENTO, "Chamado 10", "Verificação de segurança", tec10, cli10);
 
 		tecnicoRepository.saveAll(Arrays.asList(tec1, tec2, tec3, tec4, tec5, tec6, tec7, tec8, tec9, tec10));
-		clienteRepository.saveAll(Arrays.asList(cli1, cli2, cli3, cli4, cli5, cli6, cli7, cli8, cli9, cli10));
+		clienteRepository.saveAll(Arrays.asList(cli1, cli2, cli3, cli4, cli5, cli6, cli7, cli8, cli9, cli10)); 
 		chamadoRepository.saveAll(Arrays.asList(c1, c2, c3, c4, c5, c6, c7, c8, c9, c10));
 	}
 }
