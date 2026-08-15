48fff4cd580a3276421b7d8ed1741523df0e8536
JacksonLuiz99 <jacksoncuiaba99@gmail.com>
2026-08-15 19:08:36 -0400

    corrige token JWT quebrado no login
    O header de resposta do login vinha como 'Autorization: Bearer' + token,
    sem espaco entre 'Bearer' e o token, e com o nome do header errado.
    Isso fazia o token retornado pelo login nao ser aceito por nenhuma rota
    protegida, ja que o filtro de autorizacao espera 'Authorization: Bearer <token>'.

--- src/main/java/com/jackson/helpdesk/security/JWTAuthenticationFilter.java
+++ src/main/java/com/jackson/helpdesk/security/JWTAuthenticationFilter.java
@@ -48,8 +48,8 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
 		
 		String username = ((UserSS) authResult.getPrincipal()).getUsername();
 		String token = jwtUtil.generationToken(username);
-		response.setHeader("access-control-expose-headers", "Autorization");
-		response.setHeader("Autorization", "Bearer" + token);
+		response.setHeader("access-control-expose-headers", "Authorization");
+		response.setHeader("Authorization", "Bearer " + token);
 
 	}
 	
